Switch between half and Full computation mode conflict fixed

probe_model.py
@@ -22,21 +22,23 @@ from compute_iou import compute_iou
 
 
 class probe_model(nn.Module):
-    def __init__(self):
+    def __init__(self,half_mode=False):
         super(probe_model,self).__init__()
         self.model = models.resnet18(pretrained=True)
 
         # Get the GPU if available
         self.device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")    
-        #self.device=torch.device("cpu")    
+        #self.device=torch.device("cpu")
+        if half_mode:
+            self.model.half()    
         # attach the model to cuda
-        #self.model.to(self.device)
+        self.model.to(self.device)
         # run in half precision modes
         #print(self.model)
 
         
-        self.dataset_path='D:\\Net\\NetDissect\\dataset\\broden1_227'
-        #self.dataset_path='broden1_227'
+        #self.dataset_path='D:\\Net\\NetDissect\\dataset\\broden1_227'
+        self.dataset_path='broden1_227'
         
         #create dataloader
         self.imLoader= imageLoader(self.dataset_path)

utility.py
@@ -53,7 +53,7 @@ class Utility ():
             for unit_num in range(images_batch.shape[1]):
                 current_img=cv2.resize((images_batch[im_num,unit_num,:,:]).astype(np.float32), dsize=(113, 113), interpolation=cv2.INTER_CUBIC)
                 feature_mask[im_num,unit_num,:,:]=current_img>=tk[unit_num]
-        print("Feature Mask Generation Done.")
+        #print("Feature Mask Generation Done.")
         return feature_mask
     
     @staticmethod