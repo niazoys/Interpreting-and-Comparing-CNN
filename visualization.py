1de68712f0bdf4bb98f4d63c0301bc52fe1f76f0
ZHTushar23 <zahidhassantushar@gmail.com>
2021-01-02 12:51:21 +0100

    .

--- visualization.py
+++ visualization.py
@@ -256,8 +256,8 @@ class visualize_network():
 
 if __name__ == "__main__":
     # a = visualize_network('alexnet')
-    # a = visualize_network('resnet18')
-    a = visualize_network('vgg11')
+    a = visualize_network('resnet18')
+    # a = visualize_network('vgg11')
 
     # a.vis_iou_score_dist_per_layer()
     # a.vis_concept_dist_per_layer()
